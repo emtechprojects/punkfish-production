8bd7bf8f6a083f983627f1819137c6b2eaee9af3
OnlineProjectsGit <emtech.projects.live@gmail.com>
2024-02-05 11:39:36 +1100

    Complete Production Project. V1

---
+++ public/js/script.js
@@ -0,0 +1,58 @@
+window.addEventListener("scroll", function () {
+    const parallax = document.querySelector('.parallax')
+    const aboutUs = document.querySelector('.about-us')
+    let x = window.scrollY
+    // HIDE PARALLAX
+    if (x > 420) {
+        parallax.classList.add("active")
+    } else {
+        parallax.classList.remove("active")
+    }
+    // SHOW ABOUT US
+    if (x > 600) {
+        aboutUs.style.visibility = "visible"
+    } else {
+        aboutUs.style.visibility = "hidden"
+    }
+})
+
+const userEmail = document.querySelector("[data-user-email]")
+
+document.querySelector('.subscribe-btn').addEventListener('click', function () {
+    const elem = document.querySelector('.subscribe')
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+    if (!validateEmail(userEmail.value)) {
+        alert("Please enter a valid email address.")
+    } else {
+        elem.children[0].style.display = 'none'
+        elem.children[1].style.display = 'none'
+        elem.children[2].style.display = 'block'
+        setTimeout(() => {
+            elem.style.display = 'none'
+        }, 2000)
+        const data = validateEmail(userEmail.value)
+        fetch('/subscribe', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userEmail: data })
+        })
+            .then(async res => {
+                if (res.ok) return res.json()
+                const json = await res.json()
+                return await Promise.reject(json)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+        console.log("Subscribe event!")
+    }
+})
---
+++ server.mjs
@@ -0,0 +1,58 @@
+import dotenv from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+}
+
+import express from 'express'
+import nodemailer from "nodemailer"
+import path from 'path'
+
+const port = process.env.PORT
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get('/', function (req, res) {
+    res.render(path.join(process.cwd(), '/public/index.html'))
+})
+
+app.get('/index', function (req, res) {
+    res.render(path.join(process.cwd(), '/public/index.html'))
+})
+
+app.post('/subscribe', async function (req, res) {
+    const d = new Date()
+    let timeNow = d.toString()
+    const userEmail = req.body.userEmail[0]
+    console.log(userEmail)
+
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.LOGIN,
+            pass: process.env.LOGKEY
+        },
+    });
+
+    await transporter.sendMail({
+        from: userEmail, // sender address
+        to: process.env.HOSTEMAIL, // list of receivers
+        subject: "It actually works ✔✔", // Subject line
+        text: `Congratulations, the code ran successfuly and an email was sent!\nUser: ${userEmail}\nAt: ${timeNow}`, // plain text body
+      })
+      .then(info=>console.log("Message sent with ID: %s", info.messageId))
+      .catch(console.error)
+})
+
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`Error trying to connect to port`)
+    } else {
+        console.log(`Successful connection to port ${port}`)
+    }
+})
